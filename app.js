08cdced52b7e2b76be22ef37b57fc2ecf422d66a
pawan-0101 <pawancod01@gmail.com>
2025-09-24 19:04:54 +0530

    build a middleware and controllers

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const path = require('path');
-const db = require('./config/mongoose-connect');
-const ownersRouter = require('./Routes/owners.route');
-const usersRouter = require('./Routes/users.route');
-const productsRouter = require('./Routes/product.route');
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
-app.set('view engine','ejs');
-app.use("/owners",ownersRouter);
-app.use("/users",usersRouter);
-app.use("/products",productsRouter);
-//home route for testing the server
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
 
-// app.get('/',(req,res)=>{
-//     res.send('index');
-// });
+require("dotenv").config();
+
+const ownersRouter = require("./Routes/owners.route");
+const productsRouter = require("./Routes/product.route");
+const usersRouter = require("./Routes/users.route");
+const indexRouter = require("./Routes/index");
+
+const db = require("./config/mongoose-connect");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+  })
+);
+app.use(flash());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
 app.listen(3000);
